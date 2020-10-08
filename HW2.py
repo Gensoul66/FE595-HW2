80acfacf5755f5dc64f03da17bb80b9fbc26c796
Gensoul66 <460856452@qq.com>
2020-10-08 02:30:02 -0400

    Extract company name and company purpose

--- HW2.py
+++ HW2.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import numpy as np
+from bs4 import BeautifulSoup
 
 def main():
 
@@ -12,13 +13,15 @@ def main():
 
         resp = requests.get("http://3.95.249.159:8000/random_company")
 
-        # Split the text from the title
-        text_name = resp.text.split('Name:')
-        text_purpose = resp.text.split('Purpose:')
+        my_resp = BeautifulSoup(resp.text, 'html.parser').getText()
 
-        # Split the text from </li>
-        text_name_2 = text_name[1].split('</li>')[0]
-        text_purpose_2 = text_purpose[1].split('</li>')[0]
+        # Split the text from the title
+        text_name = my_resp.split('Name: ')
+        text_purpose = my_resp.split('Purpose: ')
+
+        # Split the text from </n>
+        text_name_2 = text_name[1].split('\n')[0]
+        text_purpose_2 = text_purpose[1].split('\n')[0]
 
         company_name = np.append(text_name_2, company_name)
         purpose = np.append(text_purpose_2, purpose)
